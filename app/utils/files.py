07ca20c9314eb7e87b7bd79213a96fe85d2bc675
daggerTECH <53368091+daggerTECH@users.noreply.github.com>
2026-01-02 18:13:28 +0800

    Create files.py

---
+++ app/utils/files.py
@@ -0,0 +1,11 @@
+from flask import current_app
+
+def allowed_file(filename):
+    return (
+        "." in filename
+        and filename.rsplit(".", 1)[1].lower()
+        in current_app.config.get(
+            "ALLOWED_EXTENSIONS",
+            {"png", "jpg", "jpeg", "gif"}
+        )
+    )
